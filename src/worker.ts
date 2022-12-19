8200eaccebb0e1d6c7bad8113f7f2de33d46263c
Ildar Sharafeev <i_sharafeev@apple.com>
2022-12-19 14:14:15 -0800

    Change implementation of worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,9 +1,12 @@
 const maxLimit = 1_000_000;
 let n1 = 0,
-  n2 = 1;
+  n2 = 1,
+  iteration = 0;
 console.log("Starting fibonacci worker");
-while (n1 <= maxLimit) {
+console.time("fibonacci");
+while (++iteration <= maxLimit) {
   [n2, n1] = [n1 + n2, n2];
 }
 
 console.log("Fibonacci result: ", n1);
+console.timeEnd("fibonacci");
